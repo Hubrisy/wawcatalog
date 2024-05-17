e98c5fcdacde1e2e193534c86f66273001ad2c8e
Hubrisy <ahtaras67@gmail.com>
2024-05-17 09:35:03 +0300

    new update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Shop from './components/shop/Shop';
 
 import { BrowserRouter } from 'react-router-dom';
 import Footer from './components/footer/Footer';
+import { Helmet } from 'react-helmet';
 
 function App() {
  
@@ -38,9 +39,33 @@ function App() {
   }
 
 
-  return (
+  return ( 
     <BrowserRouter>
+
+
+
     <div className="App">
+
+    <Helmet>
+    <script>
+      {`
+        !function(f,b,e,v,n,t,s){ 
+          if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '864635952168755');
+          fbq('track', 'PageView');
+      `}
+</script>
+<noscript>
+{`<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=864635952168755&amp;ev=PageView&amp;noscript=1"/>`}
+</noscript>
+    </Helmet>
+
       <div className={applyVisible ? 'apply visible' : 'apply'}>
         <div className='apply_text'>
           Товар додано!
